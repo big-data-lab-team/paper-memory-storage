b26dca96f639777ec1ac153f8d94c77256fb9bf6
Tristan Glatard <tristan.glatard@concordia.ca>
2019-09-04 09:19:09 -0400

    updates in analysis script

--- experiments/bids-app-example/analyze_results.sh
+++ experiments/bids-app-example/analyze_results.sh
@@ -2,18 +2,17 @@
 
 set -e
 
-if [ $# != 2 ]
+if [ $# != 1 ]
 then
-  echo "usage: $0 <exp_name> <n_jobs>"
+  echo "usage: $0 <exp_name>"
   exit 1
 fi
 
 exp_name=$1
-n_jobs=$2
 START=$(grep 'START DATE' ${exp_name}/logs/*.log | sort -g | head -1 | awk -F ':' '{print $3}' )
 END=$(grep 'END DATE' ${exp_name}/logs/*.log | sort -g -r | head -1 | awk -F ':' '{print $3}' )
 MAKESPAN=$(( ${END} - ${START} ))
-echo "Makespan: ${MAKESPAN} seconds"
+echo "${MAKESPAN}" > ${exp_name}/makespan_seconds
 
 task_file="${exp_name}/tasks.json"
 echo '{ "tasks": [' > ${task_file}
@@ -27,7 +26,6 @@ do
     echo "," >> ${task_file}
   fi
   first=false
-  task_number=$(( $i % ${n_jobs} ))
   i=$(( $i + 1 ))
   job_name=$(basename ${log} | awk -F '.' '{print $1}')
   start=$(grep 'START DATE' ${log} | awk -F ':' '{print $2}')
@@ -35,8 +33,12 @@ do
   end=$(grep 'END DATE' ${log} | awk -F ':' '{print $2}')
   end=$(date -d @${end})  # convert from unix timestamp to date
   code=$(grep -A 1 -i "exit code" ${log} | tail -1 | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g")
-  echo "  { \"Task\": \"${task_number}\", \"Start\": \"${start}\", \
+  real=$(awk '$1=="real" {print $2}' ${log})
+  user=$(awk '$1=="user" {print $2}' ${log})
+  sys=$(awk '$1=="sys" {print $2}' ${log})
+  echo "  { \"Start\": \"${start}\", \
             \"Finish\": \"${end}\", \"Name\": \"${job_name}\", \
-            \"Exit code\": \"${code}\" } " >> ${task_file}
+            \"Exit code\": \"${code}\", \"user time\": \"${user}\",\
+            \"real time\": \"${real}\", \"system time\": \"${sys}\" } " >> ${task_file}
 done
 echo '] }' >> ${task_file}
