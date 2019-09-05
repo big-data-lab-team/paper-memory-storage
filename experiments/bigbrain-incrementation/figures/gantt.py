a1968ad0d1228239f8638437595d7d8a8e8f31e8
valhayot <valeriehayot@gmail.com>
2019-09-04 23:59:36 -0400

    fixed gantt charts

--- experiments/bigbrain-incrementation/figures/gantt.py
+++ experiments/bigbrain-incrementation/figures/gantt.py
@@ -3,9 +3,10 @@
 import sys
 import pandas as pd
 import matplotlib.pyplot as plt
+from os import path as op
 
 
-def gantt(df):
+def gantt(df, data_file):
     color = {"read_file":"turquoise", "increment_file":"crimson",
              "write_file":"purple"}
 
@@ -17,22 +18,22 @@ def gantt(df):
         labels.append(task[0])
         for r in task[1].groupby("Task"):
             data = r[1][["Start", "Duration"]]
-            print(data, r[0])
             ax.broken_barh(data.values, (i-0.4,0.8), color=color[r[0]] )
 
-    ax.set_yticks(range(len(labels)))
-    ax.set_yticklabels(labels) 
+    #ax.set_yticks(range(len(labels)))
+    #ax.set_yticklabels(labels) 
     ax.set_xlabel("time [s]")
     plt.tight_layout() 
-    plt.savefig('gantt.pdf')
+    plt.savefig('gantt-{}.pdf'.format(op.basename(data_file)))
 
 
 data_file = sys.argv[1]
 
 df = pd.read_csv(data_file, delim_whitespace=True,
                  names=["Task", "Start", "End", "File", "ThreadId"])
-df["Duration"] = df.End - df.Start
+df["Duration"] = df["End"] - df["Start"]
+df["Start"] = df["Start"] - df["Start"].min()
 
-gantt(df)
+gantt(df, data_file)
 
 
