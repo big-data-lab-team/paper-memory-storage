19c4903ee7c6b44d415eec4dfcabba2ff58aaea4
Tristan Glatard <tristan.glatard@concordia.ca>
2019-09-13 20:40:50 -0400

    Added disk reps

--- experiments/bids-app-example/launch-disk.sh
+++ experiments/bids-app-example/launch-disk.sh
@@ -1,19 +1,37 @@
 #!/usr/bin/env bash
 
 date
-echo DISK-REP1
+echo DISK-REP2
 echo 3 | sudo tee /proc/sys/vm/drop_caches                                       
-parallel --jobs 96 < disk-1/bosh_commands.sh                                
+parallel --jobs 96 < disk-2/bosh_commands.sh                                
 
 
 sleep 100  # cool down a little
 
-mv disk-1 ~/tristan/paper-memory-storage/experiments/bids-app-example/
+mv disk-2 ~/tristan/paper-memory-storage/experiments/bids-app-example/
     
 date
-echo DISK-REP4
+echo DISK-REP5
 echo 3 | sudo tee /proc/sys/vm/drop_caches                                       
-parallel --jobs 25 < disk-4/bosh_commands.sh  
+parallel --jobs 25 < disk-5/bosh_commands.sh  
 
-mv disk-4 ~/tristan/paper-memory-storage/experiments/bids-app-example/
+mv disk-5 ~/tristan/paper-memory-storage/experiments/bids-app-example/
     
+sleep 100  # cool down a little
+
+date
+echo DISK-REP3
+echo 3 | sudo tee /proc/sys/vm/drop_caches                                       
+parallel --jobs 96 < disk-3/bosh_commands.sh                                
+
+
+sleep 100  # cool down a little
+
+mv disk-3 ~/tristan/paper-memory-storage/experiments/bids-app-example/
+    
+date
+echo DISK-REP6
+echo 3 | sudo tee /proc/sys/vm/drop_caches                                       
+parallel --jobs 25 < disk-6/bosh_commands.sh  
+
+mv disk-6 ~/tristan/paper-memory-storage/experiments/bids-app-example/
